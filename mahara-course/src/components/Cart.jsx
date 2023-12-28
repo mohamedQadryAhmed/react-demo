dd00597f6ea368fff5420d38c83ba28b9d0b7e98
mohamedQadryAhmed <mohamedQadryAhmed@gmail.com>
2023-12-28 12:03:30 +0200

    test

--- mahara-course/src/components/Cart.jsx
+++ mahara-course/src/components/Cart.jsx
@@ -2,6 +2,7 @@ const Cart = () => {
     return ( 
     <>
         <h1>Cart</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat quas, eveniet et omnis ipsum necessitatibus fuga magnam deleniti quo? Asperiores explicabo, sint voluptates soluta veritatis totam vero perferendis quas maiores!</p>
     </>
      );
 }
